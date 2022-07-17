d8a1dbee3550da3c0f0cfbd3525bdbeb624abfef
Piyush Aggarwal <27003616+ThePiyushAggarwal@users.noreply.github.com>
2022-07-17 23:52:41 +0530

    v6- added products on home page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,7 @@
 import Header from './components/Header'
 import Footer from './components/Footer'
 import Container from 'react-bootstrap/Container'
+import Home from './pages/Home'
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <main className='py-3'>
         <Container>
           <h1>Welcome to Proshop</h1>
+          <Home />
         </Container>
       </main>
       <Footer />
---
+++ frontend/src/pages/Home.js
@@ -0,0 +1,20 @@
+import products from '../products'
+import Row from 'react-bootstrap/Row'
+import Col from 'react-bootstrap/Col'
+import Product from '../components/Product'
+
+function Home() {
+  return (
+    <>
+      <h1>Latest Products</h1>
+      <Row>
+        {products?.map((product) => (
+          <Col sm={12} md={6} lg={4} xl={3}>
+            <Product product={product} />
+          </Col>
+        ))}
+      </Row>
+    </>
+  )
+}
+export default Home
