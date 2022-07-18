66f38ebab8b76ee040deb9ebb84faaa91c0746a2
Piyush Aggarwal <27003616+ThePiyushAggarwal@users.noreply.github.com>
2022-07-18 16:44:39 +0530

    v12- created backend API for getting data

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,21 @@
 const express = require('express')
 const app = express()
 const PORT = process.env.PORT || 5000
+const products = require('./data/products')
+
+app.get('/', (req, res) => {
+  res.send('Welcome to ProShop API')
+})
+
+// All Products JSON
+app.get('/api/products', (req, res) => {
+  res.send(products)
+})
+
+// Single Product
+app.get('/api/products/:id', (req, res) => {
+  const product = products.find((p) => p._id === req.params.id)
+  res.send(product)
+})
 
 app.listen(PORT, () => console.log(`Server running on ${PORT}`))
