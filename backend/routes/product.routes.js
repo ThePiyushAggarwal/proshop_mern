95264fc549f6c6d3a9a9ab63d55e84c441c58f28
Piyush Aggarwal <27003616+ThePiyushAggarwal@users.noreply.github.com>
2022-07-18 22:18:22 +0530

    v23- fetching from database & routes

---
+++ backend/routes/product.routes.js
@@ -0,0 +1,31 @@
+const express = require('express')
+const router = express.Router()
+const Product = require('../models/product.model')
+const asyncHandler = require('express-async-handler')
+
+// @desc Fetch all products
+// @route GET /api/products/
+// @access Public
+router.get(
+  '/',
+  asyncHandler(async (req, res) => {
+    const products = await Product.find({})
+    res.send(products)
+  })
+)
+
+// @desc Fetch single product
+// @route GET /api/products/:id
+// @access Public
+router.get(
+  '/:id',
+  asyncHandler(async (req, res) => {
+    const product = await Product.findById(req.params.id)
+    if (!product) {
+      res.status(404).send({ message: 'Product not found' })
+    }
+    res.send(product)
+  })
+)
+
+module.exports = router
--- backend/server.js
+++ backend/server.js
@@ -2,7 +2,6 @@ require('dotenv').config()
 const express = require('express')
 const app = express()
 const PORT = process.env.PORT || 5000
-const products = require('./data/products')
 const cors = require('cors')
 const connectDB = require('./config/db')
 
@@ -14,16 +13,7 @@ app.get('/', (req, res) => {
   res.send('Welcome to ProShop API')
 })
 
-// All Products JSON
-app.get('/api/products', (req, res) => {
-  res.send(products)
-})
-
-// Single Product
-app.get('/api/products/:id', (req, res) => {
-  const product = products.find((p) => p._id === req.params.id)
-  res.send(product)
-})
+app.use('/api/products', require('./routes/product.routes'))
 
 app.listen(PORT, () =>
   console.log(`Server running in ${process.env.NODE_ENV} on ${PORT}`)
