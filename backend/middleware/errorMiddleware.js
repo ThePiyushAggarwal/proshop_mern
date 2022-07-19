374f125bead10411b7faeacaeea2aa9234ca1613
Piyush Aggarwal <27003616+ThePiyushAggarwal@users.noreply.github.com>
2022-07-19 11:24:37 +0530

    spelling mistake

--- backend/middleware/errorMiddleware.js
+++ backend/middleware/errorMiddleware.js
@@ -7,7 +7,7 @@ const notFound = (req, res, next) => {
 const errorHandler = (err, req, res, next) => {
   const statusCode = res.statusCode === 200 ? 500 : res.statusCode
   res.status(statusCode).json({
-    messsage: err.message,
+    message: err.message,
     stack: process.env.NODE_ENV === 'production' ? null : err.stack,
   })
 }
