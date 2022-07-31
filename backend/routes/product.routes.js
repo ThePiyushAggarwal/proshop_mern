bae1097762f4d5d93635e643850843a9f1212926
Piyush Aggarwal <27003616+ThePiyushAggarwal@users.noreply.github.com>
2022-07-31 17:46:23 +0530

    v37- Refactoring backend in MVC pattern

--- backend/routes/product.routes.js
+++ backend/routes/product.routes.js
@@ -1,32 +1,11 @@
 const express = require('express')
 const router = express.Router()
-const Product = require('../models/product.model')
-const asyncHandler = require('express-async-handler')
+const {
+  getProducts,
+  getProductById,
+} = require('../controllers/product.controller')
 
-// @desc Fetch all products
-// @route GET /api/products/
-// @access Public
-router.get(
-  '/',
-  asyncHandler(async (req, res) => {
-    const products = await Product.find({})
-    res.send(products)
-  })
-)
-
-// @desc Fetch single product
-// @route GET /api/products/:id
-// @access Public
-router.get(
-  '/:id',
-  asyncHandler(async (req, res) => {
-    const product = await Product.findById(req.params.id)
-    if (!product) {
-      res.status(404)
-      throw new Error('Product not found')
-    }
-    res.send(product)
-  })
-)
+router.get('/', getProducts)
+router.get('/:id', getProductById)
 
 module.exports = router
