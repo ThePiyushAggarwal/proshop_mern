e15068dedca8ad7a056eca83ccdf206f6849c3b4
Piyush Aggarwal <27003616+ThePiyushAggarwal@users.noreply.github.com>
2022-08-13 13:00:29 +0530

    Update Loader.jsx

--- frontend/src/components/Loader.jsx
+++ frontend/src/components/Loader.jsx
@@ -1,12 +1,12 @@
 import Spinner from 'react-bootstrap/Spinner'
 
-function Loader() {
+function Loader({ height = '100px', width = '100px' }) {
   return (
     <Spinner
       animation='border'
       role='status'
       className='mx-auto my-auto d-block'
-      style={{ height: '100px', width: '100px' }}
+      style={{ height, width }}
     >
       <span className='visually-hidden'>Loading...</span>
     </Spinner>
