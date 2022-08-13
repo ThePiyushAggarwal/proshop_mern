e8b9d49af7ebb7fa4da209e9246473da758936a9
Piyush Aggarwal <27003616+ThePiyushAggarwal@users.noreply.github.com>
2022-08-13 14:42:41 +0530

    v80- Configured multer

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,7 @@ const express = require('express')
 const app = express()
 const PORT = process.env.PORT || 5000
 const cors = require('cors')
+const path = require('path')
 const connectDB = require('./config/db')
 const { notFound, errorHandler } = require('./middleware/errorMiddleware')
 
@@ -21,10 +22,13 @@ app.get('/', (req, res) => {
 app.use('/api/products', require('./routes/product.routes'))
 app.use('/api/users', require('./routes/user.routes'))
 app.use('/api/orders', require('./routes/order.routes'))
+app.use('/api/uploads', require('./routes/upload.routes'))
 
 app.get('/api/config/paypal', (_req, res) => {
   res.send(process.env.PAYPAL_CLIENT_ID)
 })
+
+app.use('/uploads', express.static(path.join(__dirname, '/uploads')))
 
 // Error Handler
 app.use(notFound)
